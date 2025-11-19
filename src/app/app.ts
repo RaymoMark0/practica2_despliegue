bdd1c578f7cde55dadc0c1daa8fbe18437922da2
RaymoMark0 <jrm24gr046@student.esgerencia.com>
2025-11-19 13:01:25 +0100

    create calendario

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Calendario } from './calendario/calendario';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Calendario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
