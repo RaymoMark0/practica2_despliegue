af617a11ab6ee30784bcfc738283766fc3273ed5
RaymoMark0 <jrm24gr046@student.esgerencia.com>
2025-11-19 13:26:20 +0100

    hotfix fix

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, practica_final_despliegue');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Prueba inicial cambio rama feature fase2');
   });
 });
